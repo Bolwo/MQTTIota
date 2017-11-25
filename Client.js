94b3112daf2b5b53c1e04c39c6d225977dd5c79d
jacovanc <jacovancr@gmail.com>
2017-11-25 17:08:09 +0000

    Included Iota. Needs to be connected to node with attachToTangle enabled

--- Client.js
+++ Client.js
@@ -2,6 +2,20 @@
 var logDiv = document.getElementById('log');
 
 MQTTConnect();
+var iota = IotaConnect();
+
+function IotaConnect(){
+   return new IOTA({
+        'provider': 'http://service.iotasupport.com:14265'
+    });
+    iota.api.getNodeInfo(function(error, success) {
+        if (error) {
+            console.error(error);
+        } else {
+            console.log(success);
+        }
+    }) 
+}
 
 function MQTTConnect() {
     // Create a client instance: Broker, Port, Websocket Path, Client ID
@@ -19,7 +33,6 @@ function MQTTConnect() {
     // Called when the connection is made
     function onConnect(){
         console.log("Connected!");
-        PublishMessage();
     }
 
     // Connect the client, providing an onConnect callback
@@ -29,14 +42,38 @@ function MQTTConnect() {
 }
 
 function SendMessage() {
-    PublishMessage();
+    var id = "Test message 123"
+    PublishMQTTMessage(id);
+    CreateIotaTransaction(id);
 }
 
-function PublishMessage(){
+function PublishMQTTMessage(id){
     // Publish a Message
-    var message = new Paho.MQTT.Message("Test payload, identifier here");
+    var message = new Paho.MQTT.Message(id);
     message.destinationName = "/IotaTransaction";
-    message.qos = 1;
 
     client.send(message);
+}
+
+function CreateIotaTransaction(id){
+    // the message which we will send with the transaction
+    var messageToSend = {
+        'name': '/IotaTransaction',
+        'message': id
+    }
+    // Stringify to JSON 
+    var messageStringified = JSON.stringify(messageToSend);
+    // Convert the string to trytes 
+    var messageTrytes = iota.utils.toTrytes(messageStringified);
+
+    var transfer = [{
+        'address': "HZPUPWANZVOLX9ALPHUSDMIJJLFWABTOGY9UPLQRCFFOMOWJCKUETHEIIQRPREXYXJDOIOPROU9HIHDXD",
+        'value': 0,
+        'message': messageTrytes
+    }]
+    // We send the transfer from this seed, with depth 4 and minWeightMagnitude 18
+    iota.api.sendTransfer("ABCDEFG", 4, 18, transfer, function(e, bundle) {
+        if (e) throw e;
+        console.log("Successfully sent your transfer: ", bundle);
+})
 }
